b8058a26fed1bcb0bc896433e133c0cf7c8dabec
Ken C.Y. Leung <kenleung5e28@gmail.com>
2022-03-06 02:47:12 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use clap::{App, Arg};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -9,7 +10,39 @@ pub struct Config {
     number_nonblank_lines: bool,
 }
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Ken Leung <kenleung5e28@gmail.com")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .required(true)
+                .min_values(1)
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .short("n")
+                .help("Number lines")
+                .takes_value(false)
+        )
+        .arg(
+            Arg::with_name("number_nonblank_lines")
+                .short("b")
+                .help("Number nonblank lines")
+                .takes_value(false)
+        )
+        .get_matches();
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
     Ok(())
 }
