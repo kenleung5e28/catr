3029c62b9afb977b5e5a388a52d24f75180e1593
Ken Leung <kenleung5e28@gmail.com>
2022-03-06 21:28:06 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 use clap::{App, Arg};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
 
 #[derive(Debug)]
 pub struct Config {
@@ -19,18 +28,21 @@ pub fn get_args() -> MyResult<Config> {
             Arg::with_name("files")
                 .value_name("FILE")
                 .help("Input file(s)")
+                .multiple(true)
                 .default_value("-")
                 .min_values(1)
         )
         .arg(
             Arg::with_name("number_lines")
                 .short("n")
+                .long("number")
                 .help("Number lines")
                 .takes_value(false)
         )
         .arg(
             Arg::with_name("number_nonblank_lines")
                 .short("b")
+                .long("number-nonblank")
                 .help("Number nonblank lines")
                 .takes_value(false)
                 .conflicts_with("number_lines")
@@ -44,6 +56,11 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
 }
